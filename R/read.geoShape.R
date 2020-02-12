af8f1d67291c9a3cbe47a50d91ada1916fd0aab8
yiluheihei <yiluheihei@gmail.com>
2020-02-12 23:50:26 +0800

    geo json data clean

--- R/read.geoShape.R
+++ R/read.geoShape.R
@@ -14,72 +14,84 @@
 ##'   plot(map)
 ##' }
 ##' @export
-read.geoShape = function(txt){
-
+read.geoShape <- function (txt) {
   raw = jsonlite::fromJSON(txt)
-  # Polygons part
 
-  ployList = lapply(raw$features$geometry$coordinates, function(x){
-    if(class(x) == "array"){
+  # remove the redundant data
+  datPart <- raw$features$properties
+  real_indx <- !sapply(datPart$name, function(x) x == "" || is.null(x) || is.na(x))
+  datPart = datPart[real_indx, ]
+  ployList = lapply(raw$features$geometry$coordinates[real_indx], function(x) {
+    if (class(x) == "array") {
       a = as.vector(x)
-      dim(a) = c(length(a)/2,2)
-      # if(length(dim(x))==3){
-      #   Sr = (sp::Polygon(x[1,,])
-      # }else{
-      #   Sr = sp::Polygon(x)
-      # }
+      dim(a) = c(length(a)/2, 2)
       Sr = sp::Polygon(a)
       Sp = sp::Polygons(list(Sr), "namei")
       return(Sp)
-    }else{
-      if(any(sapply(x, class) == 'list')){
+    }
+    else {
+      if (any(sapply(x, class) == "list")) {
         whilei = 0
-        while(any(sapply(x, class) == 'list')){
-          whilei = whilei+1
-          if(whilei==10)
+        while (any(sapply(x, class) == "list")) {
+          whilei = whilei + 1
+          if (whilei == 10)
             break
-          index = which(sapply(x, class)=='list')[1]
-          x = append(x[-index],x[[index]])
+          index = which(sapply(x, class) == "list")[1]
+          x = append(x[-index], x[[index]])
         }
       }
-
-      Sr = lapply(x,function(y){
-
+      Sr = lapply(x, function(y) {
         a = as.vector(y)
-        dim(a) = c(length(a)/2,2)
+        dim(a) = c(length(a)/2, 2)
         return(sp::Polygon(a))
-        # if(length(dim(y))==3){
-        #   return(sp::Polygon(y[1,,]))
-        # }else{
-        #   return(sp::Polygon(y))
-        # }
       })
       Sp = sp::Polygons(Sr, "namei")
       return(Sp)
     }
   })
-
-  for(i in 1:length(ployList)){
+  for (i in 1:length(ployList)) {
     ployList[[i]]@ID = as.character(i)
   }
-  ployPart =  sp::SpatialPolygons(ployList, 1:length(ployList))
-
-  # dat part
-  datPart = raw$features$properties
-  if(any(sapply(datPart, class)=='list')){
-    index = which(sapply(datPart, class)=='list')
-    outlist = lapply(index, function(x){
-      out = do.call(rbind,datPart[,x])
-      colnames(out) = paste0(names(datPart)[x], 1:dim(out)[2])
-      return(out)
-    })
-    datPart = cbind(datPart, do.call(cbind, outlist))
-    datPart = datPart[, -index]
+  ployPart = sp::SpatialPolygons(ployList, 1:length(ployList))
+  # datPart = raw$features$properties
+  if (any(sapply(datPart, class) == "list")) {
+    # index = which(sapply(datPart, class) == "list")
+    # outlist = lapply(index, function(x) {
+    #   # fix for missing data
+    #   fix <- lapply(datPart[, x], function(y) {
+    #     if (length(y)) {
+    #       return(y)
+    #     } else {
+    #       return(NaN)
+    #     }
+    #   })
+    #
+    #   out = do.call(rbind, fix)
+    #   colnames(out) = paste0(names(datPart)[x], 1:dim(out)[2])
+    #   return(out)
+    # })
+    # datPart = cbind(datPart, do.call(cbind, outlist))
+    # datPart = datPart[, -index]
+    datPart <-
+      purrr::map_df(datPart, function(x) {
+        if (class(x) == "list") {
+          lapply(x, function(y) {
+            if (length(y)) {
+              y
+            } else {
+              "na"
+            }
+          })
+        } else {
+          if (class(x) == "data.frame") {
+            if (length(x) == 1) x <- x[[1]]
+            else x <- apply(x, 1, as.list)
+          }
+          x
+        }
+      })
   }
   rownames(datPart) = row.names(ployPart)
-
-
-  ex_1.7 = sp::SpatialPolygonsDataFrame(ployPart,
-                                     datPart)
+  ex_1.7 = sp::SpatialPolygonsDataFrame(ployPart, datPart)
   return(ex_1.7)
 }
